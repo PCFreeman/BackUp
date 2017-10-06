01c08069368de639e579845ed55b8367b306ee37
Rafael Salles <rafaelcsalles@hotmail.com>
2017-10-06 02:00:26 -0300

    Points generation work for odd numbers. Squared the points area

--- New Unity Project/Assets/Scripts/GeneratePoints/PointsManager.cs
+++ New Unity Project/Assets/Scripts/GeneratePoints/PointsManager.cs
@@ -71,10 +71,24 @@ public class PointsManager : MonoBehaviour {
     //Will genarate the Points Area
     private void GeneratePoints()
     {
-               
-        //Helper variable
+        //PointArea variables
+        float offset = (GameObject.Find("Canvas").GetComponent<RectTransform>().rect.height) / 540.0f;
+        int padding = 0;
+        int baseScreenResolutionHeight = 540;
+
+
+        //Lines Variables
+        float lineHeight = (baseScreenResolutionHeight - padding) / numberLines;
+        float paddingLine = 10.5f;
+
+        //Points Variables
+        int sizePoint = (int)(lineHeight * 0.7f);
+
+
+        //Helper variables
         int check = 0;
-        
+        int checkPoints;
+
         //Initialize the List that will hold PointLines
         pointsLines = new List<GameObject>();
 
@@ -86,122 +100,304 @@ public class PointsManager : MonoBehaviour {
 
         pointsArea.name = "Points Area";
 
-        pointsArea.GetComponent<BoxCollider>().size = new Vector3(Mathf.RoundToInt((GameObject.Find("Canvas").GetComponent<RectTransform>().rect.width) * (float)0.75),
-            Mathf.RoundToInt((GameObject.Find("Canvas").GetComponent<RectTransform>().rect.height) - 10.0f), pointsArea.GetComponent<BoxCollider>().size.z);
+        pointsArea.GetComponent<BoxCollider>().size = new Vector3( (baseScreenResolutionHeight - padding) * offset,
+            (baseScreenResolutionHeight - padding) * offset,
+            pointsArea.GetComponent<BoxCollider>().size.z);
 
 
 
-
-        //Variables to generate Points
-
-        int heightLines = (int)pointsArea.GetComponent<BoxCollider>().size.y / numberLines;
-
-        int heightPoints = (int)(heightLines * 0.8);
-
-        int widthPoints = heightPoints;
 
         //Change size of points prefab
-        pointsPrefab.transform.localScale = new Vector3(widthPoints, heightPoints , pointsPrefab.transform.localScale.z) ;
+        pointsPrefab.transform.localScale = new Vector3(sizePoint, sizePoint, pointsPrefab.transform.localScale.z) ;
 
         //Variables to set Points positions in a Line
+        int emptyLineAreaSize = ((int)((baseScreenResolutionHeight - 10.0) - (2 * paddingLine)) - (numberPointsInLine * sizePoint)) / (numberPointsInLine - 1);
 
-        int emptyLineAreaSize = ((int)pointsLinesPrefab.GetComponent<BoxCollider>().size.x - (numberPointsInLine * widthPoints)) / (numberPointsInLine - 1);
-
+        if(numberLines % 2 == 1)
+        { 
         
-        //For Loop to instantiate Lines
-        for(int i = 0; i < numberLines; ++i)
-        {
-            //Instantiate Line
-            GameObject line = (GameObject)Instantiate(pointsLinesPrefab, new Vector3(-20, 0, -20), Quaternion.identity);
-
-            //Set it as child of pointsArea
-            line.transform.parent = GameObject.Find("Points Area").transform;
-
-            line.name = "Line " + i.ToString();
-
-            //Set line height
-            line.GetComponent<BoxCollider>().size = new Vector3(line.GetComponent<BoxCollider>().size.x, heightLines, line.GetComponent<BoxCollider>().size.z);
-
-            
-
-            
-            //Line Goes down
-            if (i % 2 == 1)
+            //For Loop to instantiate Lines
+            for(int i = 0; i < numberLines; ++i)
             {
-                check = check + 1;
-
-                int yOffset = -(heightLines * check);
-
-                line.transform.position = new Vector3(line.transform.position.x, line.transform.position.y + yOffset, line.transform.position.z);
-
-                
-            }
-            else //Line goes up
-            {
-                int yOffset = heightLines * check;
-                
-                line.transform.position = new Vector3(line.transform.position.x, line.transform.position.y + yOffset, line.transform.position.z);
-                
-            }
-
-            List<GameObject> pointsList = new List<GameObject>();
-
-            //Control variable
-            int checkPoints = 0;
-
-
-            for (int j = 0; j < numberPointsInLine; ++j)
-            {
-
                 //Instantiate Line
-                GameObject pointTemp = (GameObject)Instantiate(pointsPrefab, new Vector3(-20, 0, -20), Quaternion.identity);
+                GameObject line = (GameObject)Instantiate(pointsLinesPrefab, new Vector3(-20, 0, -20), Quaternion.identity);
 
                 //Set it as child of pointsArea
-                pointTemp.transform.parent = GameObject.Find("Line " + i.ToString()).transform;
+                line.transform.parent = GameObject.Find("Points Area").transform;
 
-                pointTemp.name = "Point " + j.ToString();
+                line.name = "Line " + i.ToString();
 
+                //Set line height
+                line.GetComponent<BoxCollider>().size = new Vector3((int)((baseScreenResolutionHeight - 10.0) - (2 * paddingLine)), lineHeight, line.GetComponent<BoxCollider>().size.z);
 
+                
 
-                if (j % 2 == 1)
+                
+                //Line Goes down
+                if (i % 2 == 1)
                 {
-                    checkPoints = checkPoints + 1;
+                    check = check + 1;
 
-                    int xOffset = -((widthPoints + emptyLineAreaSize) * checkPoints);
+                    int yOffset = (int)(-(lineHeight * check));
 
+                    line.transform.position = new Vector3(line.transform.position.x, line.transform.position.y + yOffset, line.transform.position.z);
 
-                    pointTemp.transform.position = new Vector3(pointTemp.transform.position.x + xOffset, line.transform.position.y, pointTemp.transform.position.z);
-
+                    
                 }
                 else //Line goes up
                 {
-                    int xOffset = (widthPoints + emptyLineAreaSize) * checkPoints;
+                    int yOffset = (int)(lineHeight * check);
+                    
+                    line.transform.position = new Vector3(line.transform.position.x, line.transform.position.y + yOffset, line.transform.position.z);
+                    
+                }
 
-                    pointTemp.transform.position = new Vector3(pointTemp.transform.position.x + xOffset, line.transform.position.y, pointTemp.transform.position.z);
+                List<GameObject> pointsList = new List<GameObject>();
+
+                checkPoints = 0;
+
+                if (numberPointsInLine % 2 == 1) //Odd number of points
+                {
+                    for (int j = 0; j < numberPointsInLine; ++j)
+                    {
+
+                        //Instantiate Point
+                        GameObject pointTemp = (GameObject)Instantiate(pointsPrefab, new Vector3(-20, 0, -20), Quaternion.identity);
+
+                        //Set it as child of Line
+                        pointTemp.transform.parent = GameObject.Find("Line " + i.ToString()).transform;
+
+                        pointTemp.name = "Point " + j.ToString();
+
+
+
+                        if (j % 2 == 1)
+                        {
+                            checkPoints = checkPoints + 1;
+
+                            int xOffset = -((sizePoint + emptyLineAreaSize) * checkPoints);
+
+
+                            pointTemp.transform.position = new Vector3(pointTemp.transform.position.x + xOffset, line.transform.position.y, pointTemp.transform.position.z);
+
+                        }
+                        else //Line goes up
+                        {
+                            int xOffset = (sizePoint + emptyLineAreaSize) * checkPoints;
+
+                            pointTemp.transform.position = new Vector3(pointTemp.transform.position.x + xOffset, line.transform.position.y, pointTemp.transform.position.z);
+
+                        }
+
+
+                        pointsList.Add(pointTemp);  //Temp list
+
+
+
+                        pointsLines.Add(line);      //Line List
+
+                    }
+
+                }
+                else  //Even number of points
+                {
+                    
+
+                    for (int j = 0; j < numberPointsInLine; ++j)
+                    {
+                        if(j % 2 == 1)
+                        {
+                            checkPoints = (int)((j - 1.0f) * 0.5f);
+                        }
+                        else
+                        {
+                            checkPoints = (int)(j * 0.5f);
+                        }
+                         
+
+                        //Instantiate Point
+                        GameObject pointTemp = (GameObject)Instantiate(pointsPrefab, new Vector3(-20, 0, -20), Quaternion.identity);
+
+                        //Set it as child of Line
+                        pointTemp.transform.parent = GameObject.Find("Line " + i.ToString()).transform;
+
+                        pointTemp.name = "Point " + j.ToString();
+
+                        int xOffset = (int)(((sizePoint + emptyLineAreaSize ) * (0.5f)) +  ((sizePoint + emptyLineAreaSize) * checkPoints));
+
+                        if (j % 2 == 1)
+                        {
+                            xOffset = -xOffset;
+                        }
+
+                        pointTemp.transform.position = new Vector3((pointTemp.transform.position.x + xOffset),
+                            line.transform.position.y,
+                            pointTemp.transform.position.z);
+
+                        pointsList.Add(pointTemp);  //Temp list
+
+
+
+                        pointsLines.Add(line);      //Line List
+
+
+
+                    }
 
                 }
 
 
-                pointsList.Add(pointTemp);  //Temp list
 
-                
 
-                pointsLines.Add(line);      //Line List
 
+                points.Add(pointsList);     //2D List
             }
 
-            points.Add(pointsList);     //2D List
+        }
+        else          // Even number of lines
+        {
+            //For Loop to instantiate Lines
+            for (int i = 0; i < numberLines; ++i)
+            {
+                int lineCheck;
+
+                if (i % 2 == 1)
+                {
+                    lineCheck = (int)((i - 1) * 0.5f);
+                }
+                else
+                {
+                    lineCheck = (int)(i * 0.5f);
+                }
+
+
+                //Instantiate Line
+                GameObject line = (GameObject)Instantiate(pointsLinesPrefab, new Vector3(-20, 0, -20), Quaternion.identity);
+
+                //Set it as child of pointsArea
+                line.transform.parent = GameObject.Find("Points Area").transform;
+
+                line.name = "Line " + i.ToString();
+
+                //Set line size
+                line.GetComponent<BoxCollider>().size = new Vector3((int)((baseScreenResolutionHeight - 10.0) - (2 * paddingLine)), lineHeight, line.GetComponent<BoxCollider>().size.z);
+
+                int yOffset = (int)((lineHeight * 0.5)+(lineHeight * lineCheck));
+
+
+                if (i % 2 == 1)//Line goes down
+                {
+                    yOffset = -yOffset;
+                }
+
+                //Set line position
+                line.transform.position = new Vector3(line.transform.position.x, line.transform.position.y + yOffset, line.transform.position.z);
+
+
+                List<GameObject> pointsList = new List<GameObject>();
+
+                checkPoints = 0;
+
+                if (numberPointsInLine % 2 == 1) //Odd number of points
+                {
+                    for (int j = 0; j < numberPointsInLine; ++j)
+                    {
+
+                        //Instantiate Point
+                        GameObject pointTemp = (GameObject)Instantiate(pointsPrefab, new Vector3(-20, 0, -20), Quaternion.identity);
+
+                        //Set it as child of Line
+                        pointTemp.transform.parent = GameObject.Find("Line " + i.ToString()).transform;
+
+                        pointTemp.name = "Point " + j.ToString();
+
+
+
+                        if (j % 2 == 1)
+                        {
+                            checkPoints = checkPoints + 1;
+
+                            int xOffset = -((sizePoint + emptyLineAreaSize) * checkPoints);
+
+
+                            pointTemp.transform.position = new Vector3(pointTemp.transform.position.x + xOffset, line.transform.position.y, pointTemp.transform.position.z);
+
+                        }
+                        else //Line goes up
+                        {
+                            int xOffset = (sizePoint + emptyLineAreaSize) * checkPoints;
+
+                            pointTemp.transform.position = new Vector3(pointTemp.transform.position.x + xOffset, line.transform.position.y, pointTemp.transform.position.z);
+
+                        }
+
+
+                        pointsList.Add(pointTemp);  //Temp list
+
+
+
+                        pointsLines.Add(line);      //Line List
+
+                    }
+
+                }
+                else  //Even number of points
+                {
+                    for (int j = 0; j < numberPointsInLine; ++j)
+                    {
+                        
+
+                        if (j % 2 == 1)
+                        {
+                            checkPoints = (int)((j - 1.0f) * 0.5f);
+                        }
+                        else
+                        {
+                            checkPoints = (int)(j * 0.5f);
+                        }
+
+
+                        //Instantiate Point
+                        GameObject pointTemp = (GameObject)Instantiate(pointsPrefab, new Vector3(-20, 0, -20), Quaternion.identity);
+
+                        //Set it as child of Line
+                        pointTemp.transform.parent = GameObject.Find("Line " + i.ToString()).transform;
+
+                        pointTemp.name = "Point " + j.ToString();
+
+                        int xOffset = (int)(((sizePoint + emptyLineAreaSize) * (0.5f)) + ((sizePoint + emptyLineAreaSize) * checkPoints));
+
+                        if (j % 2 == 1)
+                        {
+                            xOffset = -xOffset;
+                        }
+
+                        pointTemp.transform.position = new Vector3(pointTemp.transform.position.x + xOffset,
+                            line.transform.position.y,
+                            pointTemp.transform.position.z);
+
+                        pointsList.Add(pointTemp);  //Temp list
+
+
+
+                        pointsLines.Add(line);      //Line List
+
+
+
+                    }
+
+                }
+
+
+
+
+
+                points.Add(pointsList);     //2D List
+
+            }
         }
 
 
-        //for(int i = 0; i < points.Count;++i)
-        //{
-        //    for (int j = 0; j < points[i].Count; ++j)
-        //    {
-        //        Debug.Log("[" + i + "]" + "[" + j + "]" + points[i][j].transform.position.y);
-        //
-        //    }
-        //}
     }
 
 
