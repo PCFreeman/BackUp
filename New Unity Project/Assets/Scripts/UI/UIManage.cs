27b7fc1038c91d677179e0425fa6d0ea5806ae8e
PCFreeman <freemanfrank96@gmail.com>
2017-11-07 00:24:23 -0800

    Merge sucks

--- New Unity Project/Assets/Scripts/UI/UIManage.cs
+++ New Unity Project/Assets/Scripts/UI/UIManage.cs
@@ -35,6 +35,7 @@ public class UIManage : MonoBehaviour {
     public Text TryLimit;
     public Text nextLevel;
     public Text ShapeTimeLimit;
+    public int current;
 
     private void Awake()
     {
@@ -189,14 +190,19 @@ public class UIManage : MonoBehaviour {
 
     public void UpdateShapesTry(int n)//----------------------------------------------------Rafel
     {
-        if (TryLimit.gameObject.activeInHierarchy)
+        if (current < 0)
         {
-            TryLimit.gameObject.SetActive(false);
+            current = n;
         }
 
-        TryLimit.gameObject.SetActive(true);
-        TryLimit.text = n.ToString();
-        Invoke("DeactiveShapesTry", 1);
+        if (current != n)
+        {
+            TryLimit.gameObject.SetActive(true);
+            TryLimit.text = n.ToString();
+            Invoke("DeactiveShapesTry", 1);
+            current = n;
+        }
+
     }
 
     public void DeactiveShapesTry()
