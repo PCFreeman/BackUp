87f7c2e45f48a3c5d6d070a372d9ae095536c0c2
Rafael Salles <rafaelcsalles@hotmail.com>
2017-12-07 22:33:23 -0200

    w

---
+++ New Unity Project/Assets/Scripts/TouchManager/AreaCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaCollider : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        TouchManager.mTouchManager.SetIsDrawing(true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        TouchManager.mTouchManager.SetIsDrawing(false);
+    }
+}
